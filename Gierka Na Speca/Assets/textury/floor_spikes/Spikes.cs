c24823393d2a0093bde209624a14b45a86907d37
jerex-coder <paulow039@wp.pl>
2020-11-10 08:50:03 +0100

    Gierka-unity

---
+++ Gierka Na Speca/Assets/textury/floor_spikes/Spikes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes: MonoBehaviour
+{
+   public bool spike;
+    private Animator anim;
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+       if (collision.tag == "Player")
+        { 
+            spike = true;
+        }
+        else
+        {
+            spike = false;
+        }
+
+    }
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    { anim = GetComponent<Animator>();
+        
+        anim.SetBool("on_spikes", spike);
+        
+    }
+}
