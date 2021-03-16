176f1d8dfa798cc9607b33740d98992e6541580a
jerex-coder <paulow039@wp.pl>
2021-03-16 02:00:17 +0100

    Nowa lepsiejsza mapa pooziomy

---
+++ Assets/Bars.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bars : MonoBehaviour
+{
+    public bool bar;
+    private Animator anim;
+
+    private void Start()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            bar = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        bar = false;
+    }
+
+
+
+    void Update()
+    {
+        anim = GetComponent<Animator>();
+
+        anim.SetBool("bar", bar);
+
+    }
+}
---
+++ Assets/EyePortal2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EyePortal2 : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
